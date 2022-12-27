582e4fcfc38434e420d534fc13851e04faced7e8
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2022-12-27 15:22:40 -0500

    Upload Files
    Upload files from existing modified solo ghost.org template.

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+function initParallax() {
+    jarallax(document.querySelectorAll('.has-parallax-feed .gh-card'), {
+        speed: 0.8,
+    });
+}
+
+(function () {
+    if (!document.body.classList.contains('has-background-about')) return;
+
+    const about = document.querySelector('.gh-about');
+    if (!about) return;
+
+    const image = about.querySelector('.gh-about-image');
+
+    if (!image.naturalWidth) {
+        imagesLoaded(image, function () {
+            about.style.setProperty('--about-height', image.clientWidth * image.naturalHeight / image.naturalWidth + 'px');
+        });
+    }
+})();
+
+(function () {
+    initParallax();
+})();
+
+(function () {
+    const toggle = document.querySelector('[data-toggle-comments]');
+    if (!toggle) return;
+
+    toggle.addEventListener('click', function () {
+        document.body.classList.toggle('comments-opened');
+    });
+})();
+
+(function () {
+    const element = document.querySelector('.gh-article-excerpt');
+    if (!element) return;
+
+    let text = element.textContent;
+    const emojiRE = /\p{EPres}|\p{ExtPict}/gu;
+
+    const emojis = text.match(emojiRE);
+    if (!emojis) return;
+
+    emojis.forEach(function (emoji) {
+        text = text.replace(emoji, `<span class="emoji">${emoji}</span>`);
+    });
+
+    element.innerHTML = text;
+})();
+
+(function () {
+    pagination(true, initParallax);
+})();
